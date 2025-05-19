1d2dd7bdd7e36389bba27a4dc5d92f5b02ccd543
Paulo Henrique da Silva Júnior <132317175+paulophsj@users.noreply.github.com>
2025-05-19 20:09:28 -0300

    Retirando o padding

--- pages/vagas/index.jsx
+++ pages/vagas/index.jsx
@@ -4,7 +4,7 @@ export default function vaga_index() {
     return (
         <>
 
-            <section className="container mx-auto p-10 flex flex-col items-center gap-10">
+            <section className="container mx-auto flex flex-col items-center gap-10">
                 <ListCard />
             </section>
         </>
