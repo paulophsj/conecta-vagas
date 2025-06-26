7c4244d257e8e2fb785c4afc9c6c13ff6bfe46f7
paulophsj <snoopyphs@gmail.com>
2025-06-26 19:59:21 -0300

    Alterando limite do input de senha

--- pages/cadastro.jsx
+++ pages/cadastro.jsx
@@ -247,7 +247,7 @@ export default function candidato() {
                                                 Senha
                                             </label>
                                             <div className="relative">
-                                                <input type={showPassword ? 'text' : 'password'} minLength={6} maxLength={15} required value={password} onChange={(e) => setPassword(e.target.value)} id="password" name="password" autoComplete="new-password" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" />
+                                                <input type={showPassword ? 'text' : 'password'} minLength={6} maxLength={50} required value={password} onChange={(e) => setPassword(e.target.value)} id="password" name="password" autoComplete="new-password" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" />
                                                 <button tabIndex={-1} className={`cursor-pointer text-gray-700 absolute inset-y-0 right-0 px-3 flex items-center`} type="button" onClick={() => setShowPassword(!showPassword)}>
                                                     {
                                                         showPassword ? <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
@@ -290,7 +290,7 @@ export default function candidato() {
                                                 Confirmar Senha
                                             </label>
                                             <div className="relative">
-                                                <input type={showConfirmPassword ? 'text' : 'password'} minLength={6} maxLength={15} disabled={password.length < 6} required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} id="confirmar-password" autoComplete="new-password" name="confirmar-password" className={`w-full px-3 py-2 border ${password.length < 6 ? 'bg-gray-200 opacity-50' : ''} border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400`} />
+                                                <input type={showConfirmPassword ? 'text' : 'password'} minLength={6} maxLength={50} disabled={password.length < 6} required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} id="confirmar-password" autoComplete="new-password" name="confirmar-password" className={`w-full px-3 py-2 border ${password.length < 6 ? 'bg-gray-200 opacity-50' : ''} border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400`} />
                                                 <button tabIndex={-1} className={`cursor-pointer ${password.length < 6 ? 'text-gray-400 pointer-events-none' : 'text-gray-700 pointer-events-all'} absolute inset-y-0 right-0 px-3 flex items-center`} type="button" onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
                                                     {
                                                         showConfirmPassword ? <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
